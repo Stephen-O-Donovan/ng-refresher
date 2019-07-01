68f3a38db09882c75affe2dd906f173564b988e4
Stephen O'Donovan <booklad@gmail.com>
2019-07-01 16:10:41 +0100

    first

---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.css']
+})
+export class PersonsComponent implements OnInit, OnDestroy {
+  personList: string[];
+  isFetching = false;
+
+  private personListSubs: Subscription;
+  constructor(private prsService: PersonsService) {
+
+  }
+
+  ngOnInit(){
+    this.personListSubs = this.prsService.personsChanged.subscribe(persons =>{
+      this.personList = persons;
+      this.isFetching = false;
+    });
+    this.isFetching = true;
+    this.prsService.fetchPersons();
+  }
+
+  ngOnDestroy(){
+    this.personListSubs.unsubscribe();
+
+  }
+  onRemovePerson(personName: string){
+    this.prsService.removePerson(personName);
+  }
+}
